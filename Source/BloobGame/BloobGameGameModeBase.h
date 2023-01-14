eeae1daccfa24eb80528c14d8c219d597dfe5847
Sebbx <tutatu2@gmail.com>
2023-01-14 19:25:58 +0100

    0.Initial

---
+++ Source/BloobGame/BloobGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BloobGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLOOBGAME_API ABloobGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
